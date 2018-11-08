ffe77317ef5ea792925df4226f9df41bbea3f35f
Wei-Cheng Wu <spencerwu85@gmail.com>
2018-11-08 09:14:35 +0800

    Update optimize graph

--- benchmarks/random_generated/optimize-exp/plot.sh
+++ benchmarks/random_generated/optimize-exp/plot.sh
@@ -4,11 +4,12 @@ function plot_graph {
 	cat <<-EOF | gnuplot
 	set output "optimize.jpeg"
 	set terminal jpeg
+	set logscale xy
 	set xlabel "Optimize (s)
 	set ylabel "String (s)
-	set xr [0.0:320.0]
-	set yr [0.0:320.0]
-	set arrow from 0,0 to 320,320 nohead
+	set xr [3.0:350.0]
+	set yr [3.0:350.0]
+	set arrow from 3,3 to 350,350 nohead
 	plot "$1" using $2:$3 title "Optimize vs String"
 	EOF
 }

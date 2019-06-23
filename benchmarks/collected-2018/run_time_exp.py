65f29b976b98146054bc81e869f4150f5217beb9
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-06-23 22:33:34 +0800

    Change exp script

--- benchmarks/collected-2018/run_time_exp.py
+++ benchmarks/collected-2018/run_time_exp.py
@@ -6,51 +6,36 @@ from numpy import median
 from prettytable import PrettyTable
 
 
-def get_median(elements):
-    return round(median(elements), 3)
-
-def get_average(elements):
-    total = 0.0
-    count = 0
-    for element in elements:
-        total += element
-        count += 1
-    return round(total / count, 3)
-
-def get_array(results, key, index):
-    res = []
-    for elements in results[key]:
-        res.append(elements[index])
-    return res
-
 def get_all_avg(results):
     ret = None
     cnt = 0
-    for key, val in results.items():
+    for val in results:
         cnt += 1
         if ret is None:
             ret = val
         else:
-            for i in range(len(ret)):
+            for i in range(1, len(ret)):
                 ret[i] += val[i]
 
-    for i in range(len(ret)):
+    for i in range(1, len(ret)):
         ret[i] = round(ret[i]/cnt, 3)
 
-    print("Average: %s %s %s %s %s %s %s" % (val[0], val[1], val[2], val[3], val[4], val[5], val[6]))
+    print("Average: %s %s %s %s %s %s" % (ret[1], ret[2], ret[3], ret[4], ret[5], ret[6]))
 
-def get_max(results, index):
-    ret = None
-    for key, val in results.items():
-        if ret is None:
-            ret = (key, val)
-        else:
-            if ret[1][index] < val[index]:
-                ret = (key ,val)
+def get_sorted(results, index):
+    ret = list(results)
+    
+    for i in range(len(ret)):
+        for j in range(i, len(ret) - 1):
+            if ret[j][index] < ret[j+1][index]:
+                tmp = ret[j][index]
+                ret[j][index] = ret[j+1][index]
+                ret[j+1][index] = tmp
 
-    key, val = ret
 
-    print("%s %s %s %s %s %s %s %s" % (key, val[0], val[1], val[2], val[3], val[4], val[5], val[6]))
+    for i in range(3):
+        print("%s\t%s %s %s %s %s %s" % (ret[i][0], ret[i][1], ret[i][2], ret[i][3], ret[i][4], ret[i][5], ret[i][6]))
+
 
 
 def main():
@@ -58,8 +43,7 @@ def main():
     extracted_path = global_path + "extracted/"
     files = os.listdir(extracted_path)
 
-    groups = dict()
-    results = dict()
+    results = []
     cnt = 0
 
     for java in files:
@@ -74,6 +58,7 @@ def main():
         process_time = 0.0
         solver_time = 0.0
         total_time = 0.0
+
         for i in range(5):
             print(i, java)
             proc = Popen(cmd, shell=True, stdout=PIPE)
@@ -99,63 +84,23 @@ def main():
         total_time = round(total_time, 3)
 
         print("%s %s %s %s %s %s %s" % (compile_time, process_time, solver_time, total_time, num_line, num_variable, num_if))
-        results[java] = [compile_time, process_time, solver_time, total_time, num_line, num_variable, num_if]
+        results.append([java, compile_time, process_time, solver_time, total_time, num_line, num_variable, num_if])
+
+        cnt += 1
+        if cnt >= 4:
+            break
         
 
     print("Longest executation time:")
-    get_max(results, 3)
+    get_sorted(results, 4)
     print("Most num of lines:")
-    get_max(results, 4)
+    get_sorted(results, 5)
     print("Most num of variables:")
-    get_max(results, 5)
+    get_sorted(results, 6)
     print("Most num of if condition:")
-    get_max(results, 6)
+    get_sorted(results, 7)
+
     get_all_avg(results)
-    """
-        times = (compile_time, process_time, solver_time)
-        # Group by line numbers
-        if group in groups:
-            groups[group].append(target)
-            results[group].append(times)
-        else:
-            groups[group] = [target]
-            results[group] = [times]
-        cnt += 1
-
-    keys = sorted(groups.keys())
-
-
-    x = PrettyTable(["Lines", 
-                     "Average (Compile)", "Average (Process)", "Average (Solver)", "Average(total)"
-                     ,"Median (Compile)", "Median (Process)", "Median (Solver)", "Median (total)"
-                    ])
-
-    final_sets = dict()
-    for key in keys:
-        final = dict()
-        array = get_array(results, key, 0)
-        ca = get_average(array)
-        cm = get_median(array)
-        final["compile"] = (ca, cm)
-
-        array = get_array(results, key, 1)
-        pa = get_average(array)
-        pm = get_median(array)
-        final["process"] = (pa, pm)
-        
-        array = get_array(results, key, 2)
-        sa = get_average(array)
-        sm = get_median(array)
-        final["smt"] = (sa, sm)
-
-        final_sets[key] = final
-        line = "%s-%s" % (key*10+1, key*10+9)
-        x.add_row([line,
-                   ca, pa, sa, ca+pa+sa,
-                   cm, pm, sm, cm+pm+sm])
-
-    print(x)
-    """
 
 if __name__ == "__main__":
     main()
